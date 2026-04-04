6372145d6aae92b802aa8b553c4bd3a7c864d509
Dragan Jotanovic <draganj@gmail.com>
2026-04-04 20:24:05 +0100

    Initial zig bootstrap

--- bindings/kdb/shm-sender.q
+++
@@ -1,9 +0,0 @@
-\l shm.q
-
-.shmipc.init[`:java/queue;`text;5;1b];
-
-\t 1000
-
-started:string .z.p;
-
-.z.ts:{.shmipc.append[`:java/queue;" -> " sv (started;string .z.p)];}
--- bindings/python/reader.py
+++
@@ -1,9 +0,0 @@
-import libchronicle
-import sys
-
-path = sys.argv[1]
-with libchronicle.Queue(path) as q:
-    with q.tailer(0) as tailer:
-        while True:
-            index, bs = tailer.collect()
-            print(f"[{index}] {bs}")
